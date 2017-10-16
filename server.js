dbcd61da92c3140b714de121babe977134b66aff
Kellana <kellyg15@uia.no>
2017-10-16 16:01:48 +0200

    task2 in Assignement 2

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+const greetings = ["Hello", "Hi", "Hey", "Hey I am the doctor", "Hei", "May the force be with you", "Look what the cat dragged in!", "Y-ellow", "Oh no, not you again!"];
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static('public'))
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/greet', function (req, res) {
+    
+    let randomIndex = Math.floor(Math.random()*greetings.length);
+
+    res.send(greetings[randomIndex] + ", " + req.query.navn);
+})
+
+/*app.get('/greet/:id', function (req, res) {
+  
+    let randomIndex = Math.floor(Math.random()*greetings.length);
+ //   let greetingsname = greetings[randomIndex] + " " + req.params.id;
+
+   res.send("greet" + " " + req.params.id);
+}) */
+
+app.listen(app.get("port"), function () {
+  console.log('Example app listening on port 8080!')
+})
+
+
+
+
